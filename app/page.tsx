595c7dedf745cea8012a18cf020670f98c1b483a
Augustin <jacquesson.augustin@gmail.com>
2025-10-17 16:09:01 +0200

    Add Gallery component to homePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Features from "@/components/Features";
+import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
     <Hero />
     <Features />
+    <Gallery />
     </>
   );
 }
