ec339d09b56d06c25382ae22fa93b5ecbd35f511
Augustin <jacquesson.augustin@gmail.com>
2025-11-19 10:08:52 +0100

    fix bug

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,18 +1,33 @@
-import { createServerClient } from '@supabase/ssr' 
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
 
-export const createSupabaseServerClient = () => {
-  const cookieStore = cookies() 
-  
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-    throw new Error("Missing Supabase environment variables (URL or ANON_KEY)");
-  }
+export function createServerSupabase() {
+  const cookieStore = cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: cookieStore 
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: any) {
+          cookieStore.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: any) {
+          cookieStore.set({
+            name,
+            value: "",
+            ...options,
+            maxAge: 0,
+          });
+        },
+      },
     }
-  )
+  );
 }
