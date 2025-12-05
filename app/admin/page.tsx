3e1296d4beccfd917590b271d35adff692646e13
Augutin51 <jacquesson.augustin@gmail.com>
2025-12-05 13:14:27 +0100

    admin inputs styling

--- app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { useAdmin } from "@/components/AdminProvider";
-
-export default function AdminPage() {
-  const isAdmin = useAdmin();
-  return <div>{isAdmin ? "Admin" : "Not admin"}</div>;
-}
